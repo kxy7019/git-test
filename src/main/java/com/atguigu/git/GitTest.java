e9a2538371cac6341f89dc531e2a21fb7aae8107
Layne <kxy7019@163.com>
2022-04-17 22:47:09 +0800

    first commit

---
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -0,0 +1,11 @@
+package com.atguigu.git;
+
+/**
+ * @author kxy
+ * @create 2022-04-17 22:37
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git");
+    }
+}
